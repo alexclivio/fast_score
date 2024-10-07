881e8ba94f03145c029af1ac2a69719f088e8371
Alex Clivio <amrclivio@gmail.com>
2024-10-06 17:46:25 -0700

    add HTTPoison and Poison dependencies, add function that fetching comptetion data from football-data api, render competition in index.html

--- lib/fast_score_web/controllers/fast_score_controller.ex
+++ lib/fast_score_web/controllers/fast_score_controller.ex
@@ -1,7 +1,27 @@
 defmodule FastScoreWeb.FastScoreController do
   use FastScoreWeb, :controller
+  alias HTTPoison
+  alias Poison
 
   def index(conn, _params) do
-    render(conn, :index)
+    data = fetch_api_competition_data()
+
+    render(conn, :index, data: data)
+  end
+
+  defp fetch_api_competition_data() do
+    # make a GET request to the football-data API
+    api_url = "https://api.football-data.org/v4/competitions"
+
+    case HTTPoison.get(api_url, [{"X-Auth-Token", "a72c7951c42845dc98338929abba5ec4"}]) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        Poison.decode!(body)
+
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        IO.puts("404 Not Found")
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.puts("Error: #{reason}")
+    end
   end
 end
--- mix.exs
+++ mix.exs
@@ -55,7 +55,9 @@ defmodule FastScore.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:httpoison, "~> 2.0"},
+      {:poison, "~> 6.0"}
     ]
   end
 
