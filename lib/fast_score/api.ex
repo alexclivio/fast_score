604e1c337b9a4bbb35f101453405710ec3933523
Alex Clivio <amrclivio@gmail.com>
2025-02-24 18:18:21 -0800

    refactor api file and api call for competitions

--- lib/fast_score/api.ex
+++ lib/fast_score/api.ex
@@ -3,21 +3,24 @@ defmodule FastScore.Api do
   alias Poison
 
   def fetch_api_competition_data() do
-    # make a GET request to the football-data API
     api_url = "https://api.football-data.org/v4/competitions"
-    api_key = Application.get_env(:fast_score, :api_key)
 
-    case HTTPoison.get(api_url, [{"X-Auth-Token", api_key}]) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        body
-        |> Poison.decode!()
-        |> filter_competitions_data()
+    # Fetch the API key from the configuration
+    case Application.fetch_env!(:fast_score, :api_key) do
+      {:ok, api_key} ->
+        # Make the GET request to the API
+        case HTTPoison.get(api_url, [{"X-Auth-Token", api_key}]) do
+          {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+            body
+            |> Poison.decode!()
+            |> filter_competitions_data()
 
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        IO.puts("404 Not Found")
+          {:ok, %HTTPoison.Response{status_code: status_code}} ->
+            IO.puts("Failed with status code: #{status_code}")
 
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.puts("Error: #{reason}")
+          {:error, %HTTPoison.Error{reason: reason}} ->
+            IO.puts("Error: #{reason}")
+        end
     end
   end
 
